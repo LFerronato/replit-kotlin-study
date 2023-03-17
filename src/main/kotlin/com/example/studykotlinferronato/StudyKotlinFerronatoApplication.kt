85ddca759fe5a8b6ff6565bf22a95bb1227910d1
Lucas Ferronato <43012047+LFerronato@users.noreply.github.com>
2023-03-17 17:39:49 +0000

    update build command

---
+++ src/main/kotlin/com/example/studykotlinferronato/StudyKotlinFerronatoApplication.kt
@@ -0,0 +1,10 @@
+package com.example.studykotlinferronato
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication class StudyKotlinFerronatoApplication
+
+fun main(args: Array<String>) {
+  runApplication<StudyKotlinFerronatoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/studykotlinferronato/StudyKotlinFerronatoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.studykotlinferronato
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyKotlinFerronatoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
